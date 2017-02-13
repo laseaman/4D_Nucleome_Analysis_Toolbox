c1482d09f6d9fb9023bfbcb854d091d340cbf167
laseaman <laseaman@umich.edu>
2017-02-13 15:48:57 -0500

    First commit

---
+++ TestFile.m
@@ -0,0 +1,10 @@
+%% Hi-C analysis READ_ME
+
+% Package for Hi-C data including loading, plotting, translocation exploration, normalization, and TAD calculation.
+
+% packaged by:
+% Laura Seaman
+% PhD Candidate in Bioinformatics, University of Michigan
+% laseaman@umich.edu
+
+% to be added soon upon publication.
